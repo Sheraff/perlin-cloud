aaabee1bed7561db48cf490913c1f49c2d17c16e
Sheraff <fpellet@ensc.fr>
2022-07-06 07:44:45 +0200

    pseudorandom uses single class

--- Classes/PseudoRandomMatrix.js
+++ Classes/PseudoRandomMatrix.js
@@ -1,24 +1,18 @@
 export default class PseudoRandomMatrix {
 	constructor(dimensions) {
 		if(dimensions === 1) {
-			return new PseudoRandomVector()
+			return new Proxy(this, {
+				get(target, key) {
+					if(!target[key])
+						target[key] = Math.random()
+					return target[key]
+				}
+			})
 		}
 		return new Proxy(this, {
 			get(target, key) {
 				if(!target[key])
 					target[key] = new PseudoRandomMatrix(dimensions - 1)
-				return target[key] 
-			}
-		})
-	}
-}
-
-class PseudoRandomVector {
-	constructor() {
-		return new Proxy(this, {
-			get(target, key) {
-				if(!target[key])
-					target[key] = Math.random()
 				return target[key]
 			}
 		})
